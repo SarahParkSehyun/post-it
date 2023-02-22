6aff3af79e3022a5e3836351123b96e031149bb8
박세현 <sarah1918@naver.com>
2023-02-22 17:21:43 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
+import React from 'react';
+import styles from "./Home.module.css";
 
-function App() {
-  return (
-    <div className="threehalf">
-      <div className="yellowBackground"></div>
-      <div className='right'>
-        <div className='top'>
-          <h1 className='h1AfterLogin'>내 노트 만들기</h1>
+function Home(){
+  return(
+    <div className={styles.threehalf}>
+      <div className={styles.yellowBackground}></div>
+      <div className={styles.right}>
+        <div className={styles.top}>
+          <h1 className={styles.h1AfterLogin}>내 노트 만들기</h1>
         </div>
-        <div className='center'>
-          <h2 className='noteowner'>노트의 주인</h2>
+        <div className={styles.center}>
+          <h2 className={styles.noteowner}>노트의 주인</h2>
           <input type="text" className='textbox' maxLength="12"></input>
-          <h2 className='noteintroduce'>노트 소개</h2>
+          <h2 className={styles.noteintroduce}>노트 소개</h2>
           <input type="text" className='textbox' maxLength="30"></input>
         </div>
-        <div className='under'>
+        <div className={styles.under}>
           <a href='make_success.html'><input type="submit" className='button' value="노트 만들기"></input></a>
         </div>
       </div>
-      <div className='yellowBackground'></div>
-
-      
+      <div className={styles.yellowBackground}></div> 
     </div>
+  )
+}
+
+function App() {
+  return (
+    <Home></Home>
   );
 }
 
