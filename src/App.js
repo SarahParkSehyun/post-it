1fd0cf844fefb9f909b08557d87d76881c69758a
박세현 <sarah1918@naver.com>
2023-02-22 18:42:54 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ function Login(){
         </div>
         <div className={styles.center}>
           <h2 className={styles.noteowner}>이메일</h2>
-          <input type="text" className='textbox' maxLength="12" placeholder='이메일을 입력해주세요.'></input>
+          <form>
+          <p>
+          <input type="text" className={styles.textbox} placeholder='이메일을 입력해주세요.'></input>
+          </p>
+          </form>
           <h2 className={styles.noteintroduce}>비밀번호</h2>
-          <input type="text" className='textbox' maxLength="30" placeholder='비밀번호를 입력해주세요.'></input>
+          <form>
+          <p>
+          <input type="text" className={styles.textbox} placeholder='비밀번호를 입력해주세요.'></input>
+          </p>
+          </form>
         </div>
         <div className={styles.under}>
-          <a href='makeNote.html'><input type="submit" className='button' value="로그인"></input></a>
+          <a href='makeNote.html'><input type="submit" className={styles.button} value="로그인"></input></a>
         </div>
         <div className={styles.under}>
-          <a href='signup.html'><input type="submit" className='button' value="회원가입"></input></a>
+          <a href='signup.html'><input type="submit" className={styles.button} value="회원가입"></input></a>
         </div>
       </div>
       <div className={styles.yellowBackground}></div> 
