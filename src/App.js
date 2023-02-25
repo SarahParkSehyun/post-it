05b30ba6fc93353c383b631fdd7b869f953c4dd4
박세현 <sarah1918@naver.com>
2023-02-25 20:20:50 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import styles from "./Login.module.css";
 import homestyles from "./Home.module.css"
+import ss from "./SignUpSuccess.module.css"
 
 function Home(){
   return(
@@ -96,10 +97,30 @@ function SignUp(){
 }
 
 
+function SignUpSuccess(){
+  return(
+    <div className={ss.threehalf}>
+      <div className={ss.yellowBackground}></div>
+      <div className={ss.right}>
+        <div className={ss.top}>
+          <h1 className={ss.h1AfterLogin}>회원가입</h1>
+        </div>
+        <div className={ss.center}>
+          <h2 className={ss.noteintroduce}>회원가입 완료!</h2>
+        </div>
+        <div className={ss.under}>
+          <a href='Login.html'><input type="submit" className={ss.button} value="로그인 하러가기"></input></a>
+        </div>
+      </div>
+      <div className={ss.yellowBackground}></div> 
+    </div>
+  )
+}
+
   
 function App() {
   return (
-    <SignUp></SignUp>
+    <SignUpSuccess></SignUpSuccess>
   );
 }
 
